6c3e0b9785a3aa23c743b291c381e87510051395
This is Rude <dbauman1@swarthmore.edu>
2017-12-27 01:16:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -27,3 +27,7 @@ function removeElementFromBeginningOfArray(array){
   var newarray = array.slice(1)
   return newarray
 }
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
