8e6f062eb27c7947611febcfccb7ea51b1c3851c
This is Rude <dbauman1@swarthmore.edu>
2017-12-27 00:39:40 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ function addElementToBeginningOfArray(array,element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array,elemnt){
-
+  array.unshift(element)
 }
 function addElementToEndOfArray(){
 
